b060ac72afbc2022b73c3dbc5432a169ea864c36
Marissa Pels <marissapels@gmail.com>
2017-09-09 15:38:37 -0500

    connect to heroku

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost/nytreact");
+mongoose.connect("mongodb://heroku_cb2dljk1:25cbmhgevsql657ace8qjr2u8b@ds127564.mlab.com:27564/heroku_cb2dljk1");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
